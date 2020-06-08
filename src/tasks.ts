e11f5a16aa3e0d5ce6284e6492c61ff4ee8964a4
Luis Olivares <lvolivaresgomez@gmail.com>
2020-06-08 03:53:10 +0200

    pause, resume and errors
    Added the functions for pause, and resume the timer, and the error control

--- src/tasks.ts
+++ src/tasks.ts
@@ -11,7 +11,9 @@ export class Task extends EventEmitter {
     private timer!: NodeJS.Timeout
     private startedAt!: Date
     private endedAt!: Date
+    private paused = false
     private finished = false
+    private errors: Error[]
     //#endregion
 
     constructor(callback: (...args: any[]) => void, config: any, ...args: any[]) {
@@ -21,15 +23,29 @@ export class Task extends EventEmitter {
         this.callback = callback
         this.countdown = config.countdown
         this.args = args
+        this.errors = new Array<Error>()
+    }
+
+    //#region public interface
+    public isPaused(): boolean {
+        return this.paused
     }
 
     public isFinished(): boolean {
         return this.finished
     }
 
+    public hasErrors(): boolean {
+        return this.errors.length > 0
+    }
+
+    public errorsSize(): number {
+        return this.errors.length
+    }
+
     public run(): void {
         this.startedAt = new Date()
-        this.timer = setTimeout(            
+        this.timer = setTimeout(
             (args: any[]) => this.resolve(args),
             this.countdown,
             this.args
@@ -37,8 +53,39 @@ export class Task extends EventEmitter {
         this.emit("start")
     }
 
+    public pause(): void {
+        if (!this.isFinished || !this.isPaused()) {
+            clearTimeout(this.timer)
+            this.paused = true
+            this.emit("pause")
+        }
+    }
+
+    public resume(): void {
+        if (!this.isFinished() && this.isPaused()) {
+            this.timer = setTimeout(
+                (args: any[]) => this.resolve(args),
+                this.countdown,
+                this.args
+            )
+            this.paused = false
+            this.emit("resume")
+        }
+    }
+
+    public terminate() {
+        clearTimeout(this.timer)
+        this.endedAt = new Date()
+        this.finished = true
+        this.emit("end")
+    }
+
     public duration(): number {
         return (this.endedAt.getTime() - this.startedAt.getTime())
+    }
+
+    public getErrors(): Error[] {
+        return this.errors
     }
 
     public toString(): string {
@@ -52,6 +99,7 @@ export class Task extends EventEmitter {
             this.callback(args)
             this.emit("result")
         } catch(error) {
+            this.errors.push(error)
             this.emit("error", error)
         } finally {
             this.endedAt = new Date()
